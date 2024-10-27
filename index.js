6152bc5bef30d2eeebbaf7efdaf0bf3cafdb7a87
AlekseiGreen <120323580+AlekseiGreen@users.noreply.github.com>
2024-10-27 20:18:43 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const BUTTON = document.querySelector('.button');
+const LIST = document.querySelector('.content');
+const URL = 'https://api.thecatapi.com/v1/images/search?limit=20';
+
+
+async function getImagesAPI(list, url){
+  let response = await fetch(url);
+  let content = await response.json();
+
+  for(let key in content) {
+    list.innerHTML +=`<img src="${content[key].url}" class="image" alt="img">`
+  };
+};
+
+BUTTON.addEventListener("click", ()=>{
+  getImagesAPI(LIST, URL);
+  console.log("Сработала кнопка");
+})
